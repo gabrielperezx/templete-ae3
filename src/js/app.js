fda1ac24116d112a84fff413d1cf3240ffbc521a
gabrielperezx <gabrieloctavioperez@gmail.com>
2025-04-05 17:03:38 -0400

    Refactorización de los enlaces de navegación y mejora del diseño en varias páginas.
    - Se actualizaron los enlaces de navegación para eliminar referencias innecesarias a directorios y mantener la coherencia.
    - Se ajustó el diseño de las secciones de noticias y personal para una mejor respuesta.
    - Se mejoró el estilo de las tarjetas y se añadieron controles de paginación para una mejor experiencia de usuario.
    - Se modificó SCSS para introducir nuevos estilos para indicadores e insignias de tarjetas.

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,125 @@
 	# Funciones
 	----- ----- ----- ----- -----*/
 
+    const Calendario = () => {
+        const checkCalendario = document.querySelector('.calendar');
+        if (document.body.contains(checkCalendario)) {
+            const monthYearElement = document.getElementById('monthYear'),
+                datesElement = document.getElementById('dates'),
+                eventsContainer = document.getElementById('eventsContainer'),
+                prevBtn = document.getElementById('prevBtn'),
+                nextBtn = document.getElementById('nextBtn');
+
+            let currentDate = new Date();
+            const events = [
+                {
+                    date: '2025-04-25',
+                    title: 'Navidad',
+                    description: 'Celebración de la Navidad',
+                },
+                {
+                    date: '2025-04-30',
+                    title: 'Fin de Año',
+                    description: 'Cena de fin de año con familia',
+                },
+            ];
+
+            const updateCalendar = () => {
+                const currentYear = currentDate.getFullYear(),
+                    currentMonth = currentDate.getMonth();
+
+                const firstDay = new Date(currentYear, currentMonth, 1),
+                    lastDay = new Date(currentYear, currentMonth + 1, 0),
+                    totalDays = lastDay.getDate(),
+                    firstDayIndex = firstDay.getDay(),
+                    lastDayIndex = lastDay.getDay();
+
+                const monthYearString = currentDate.toLocaleString('default', {
+                    month: 'long',
+                    year: 'numeric',
+                });
+                monthYearElement.textContent = monthYearString;
+
+                let datesHTML = '';
+
+                for (let i = firstDayIndex; i > 0; i--) {
+                    const prevDate = new Date(currentYear, currentMonth, -i + 1);
+                    datesHTML += `<div class="calendar__date inactive">${prevDate.getDate()}</div>`;
+                }
+
+                for (let i = 1; i <= totalDays; i++) {
+                    const date = new Date(currentYear, currentMonth, i);
+                    const formattedDate = date.toISOString().split('T')[0];
+                    const hasEvent = events.some((event) => event.date === formattedDate);
+                    const activeClass =
+                        date.toDateString() === new Date().toDateString() ? 'active' : '';
+                    const eventClass = hasEvent ? 'event' : '';
+                    datesHTML += `<div class="calendar__date ${activeClass} ${eventClass}" data-date="${formattedDate}">${i}</div>`;
+                }
+
+                for (let i = 1; i <= 6 - lastDayIndex; i++) {
+                    const nextDate = new Date(currentYear, currentMonth + 1, i);
+                    datesHTML += `<div class="calendar__date inactive">${nextDate.getDate()}</div>`;
+                }
+
+                datesElement.innerHTML = datesHTML;
+
+                attachDateClickListeners();
+            };
+
+            const attachDateClickListeners = () => {
+                const dateElements = document.querySelectorAll('.calendar__date');
+                dateElements.forEach((dateElement) => {
+                    dateElement.addEventListener('click', () => {
+                        const selectedDate = dateElement.getAttribute('data-date');
+                        showEvents(selectedDate);
+                    });
+                });
+            };
+
+            const formatDate = (dateString) => {
+                const date = new Date(dateString);
+                const day = String(date.getDate() + 1);
+                const month = String(date.getMonth() + 1); // Los meses comienzan desde 0
+                const year = String(date.getFullYear()); // Año completo (YYYY)
+                return `${day}/${month}/${year}`;
+            };
+
+            const showEvents = (date) => {
+                const filteredEvents = events.filter((event) => event.date === date);
+                eventsContainer.innerHTML = '';
+
+                if (filteredEvents.length === 0) {
+                    eventsContainer.innerHTML =
+                        '<p class="m-0 fw-bold">No hay eventos para este día.</p>';
+                } else {
+                    filteredEvents.forEach((event) => {
+                        eventsContainer.innerHTML += `
+                        <div class="calendar__event">
+                            <p class="calendar__event-date">${formatDate(event.date)}</p>
+                            <p class="calendar__event-title">${event.title}</p>
+                            <p class="calendar__event-description">${
+                                event.description
+                            }</p>
+                        </div>`;
+                    });
+                }
+            };
+
+            prevBtn.addEventListener('click', () => {
+                currentDate.setMonth(currentDate.getMonth() - 1);
+                updateCalendar();
+            });
+
+            nextBtn.addEventListener('click', () => {
+                currentDate.setMonth(currentDate.getMonth() + 1);
+                updateCalendar();
+            });
+
+            updateCalendar();
+        }
+    };
+
     const LightBox = () => {
         const checkGaleria = document.getElementById('lightboxImage');
         if (document.body.contains(checkGaleria)) {
@@ -84,6 +203,8 @@
         const gliderConfig3Slides = {
             slidesToShow: 1,
             slidesToScroll: 1,
+            scrollLock: true,
+
             responsive: [
                 {
                     breakpoint: 769,
@@ -95,18 +216,17 @@
             ],
         };
 
-        const gliderConfig1Slides = {
+        const gliderConfig1SlidesBase = {
             slidesToShow: 1,
             slidesToScroll: 1,
             scrollLock: true,
-            dots: '.carousel__indicadores',
         };
 
         document.querySelectorAll('.carousel__items').forEach((carousel) => {
-            const prevButton =
-                carousel.parentElement.querySelector('.carousel__anterior');
-            const nextButton =
-                carousel.parentElement.querySelector('.carousel__siguiente');
+            const container = carousel.closest('.carousel');
+            const prevButton = container.querySelector('.carousel__anterior');
+            const nextButton = container.querySelector('.carousel__siguiente');
+            const dots = container.querySelector('.carousel__indicadores');
 
             new Glider(carousel, {
                 ...gliderConfig3Slides,
@@ -114,21 +234,23 @@
                     prev: prevButton,
                     next: nextButton,
                 },
+                dots: dots,
             });
         });
 
         document.querySelectorAll('.carousel__lista-1').forEach((carousel) => {
-            const prevButton =
-                carousel.parentElement.querySelector('.carousel__anterior');
-            const nextButton =
-                carousel.parentElement.querySelector('.carousel__siguiente');
+            const container = carousel.closest('.carousel');
+            const prevButton = container.querySelector('.carousel__anterior');
+            const nextButton = container.querySelector('.carousel__siguiente');
+            const dots = container.querySelector('.carousel__indicadores');
 
             new Glider(carousel, {
-                ...gliderConfig1Slides,
+                ...gliderConfig1SlidesBase,
                 arrows: {
                     prev: prevButton,
                     next: nextButton,
                 },
+                dots: dots,
             });
         });
     };
@@ -142,5 +264,6 @@
         LightBox();
         BotonScrollTop();
         MenuEstatico();
+        Calendario();
     });
 })();
